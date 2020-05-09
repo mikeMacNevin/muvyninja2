bc7fc28e6334019b7fc53cc07019e5d21f015a70
Mike MacNevin <mikemacnevin@gmail.com>
2020-05-09 17:36:27 -0400

    post form sending to app.js corretly

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ require('./config/passport')(passport);
 const auth = require('./routes/auth');
 const index = require('./routes/index');
 const browse = require('./routes/browse');
+const search= require('./routes/search');
 
 // Load Keys
 const keys = require('./config/keys');
@@ -79,10 +80,20 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/find', (req, res) => {
-  res.render('find');
+app.get('/search', (req, res) => {
+  res.render('search');
 });
 
+app.post('/search', (req, res) => {
+  console.log(req.body.movieTitle);
+  let movie = req.body.movieTitle;
+
+  // const url = 'https://api.themoviedb.org/3/discover/movie?api_key=52355b2a478c82d6bfe5a57afff6c916&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
+
+  res.render('search', {
+    'movie' : movie
+  })
+})
 // app.get('/', (req, res) => {
 //   res.render('browse');
 // });
---
+++ routes/search.js
@@ -0,0 +1,20 @@
+// const express = require('express');
+// const router = express.Router();
+// const keys = require('../config/keys');
+// const bodyParser = require('body-parser');
+// const request = require('request');
+
+// router.get('/', (req, res) => {
+//     const title = 'seefilm';
+//     console.log(title);
+//     res.render('search', {
+    
+      
+//     });
+//   });
+
+// router.post('/movie-title', (req, res) => {
+//     console.log(res.body);
+//    res.redirect('/')
+// });
+// module.exports = router;
